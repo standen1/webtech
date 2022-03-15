8e94edc249a3ef1d6659f071aef03b107bcb873d
Sean Standen <standen.sean@gmail.com>
2022-03-15 12:55:39 -0700

    Setting up the Navigation Menu Version 2.

---
+++ src/components/Layout/Nav/NavMenu.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export default function NavMenu() {
+  return (
+    <div></div>
+  )
+}
