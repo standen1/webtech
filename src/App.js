a4ca92d4a785ac9257b17db5861880339cca6343
Sean Standen <standen.sean@gmail.com>
2022-03-17 10:29:05 -0700

    Added Portfolio to Routes in App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from 'styled-components';
 import { MotionConfig } from "framer-motion"
 
+//Theme files
 import { theme } from './styles/Theme';
 import { GlobalStyles } from './styles/Globals';
+//Layout
 import Layout from './components/Layout/Layout';
-
+//Page Components
 import Home from './pages/Home';
 import About from './pages/About';
+import Portfolio from './pages/Portfolio';
 
 function App() {
   return (
@@ -18,6 +21,7 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/" element={<Home />} />
           </Routes>
         </Layout>
