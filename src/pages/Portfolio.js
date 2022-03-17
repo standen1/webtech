a05b3e55f1cbaa624d45885c1c0204f1f9436888
Sean Standen <standen.sean@gmail.com>
2022-03-17 10:24:49 -0700

    initial portfolio page setup

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+export default function Portfolio() {
+  return (
+    <main>
+        <Div>
+            
+        </Div>
+    </main>
+  )
+}
+
+const Div = styled.div`
+
+`;
