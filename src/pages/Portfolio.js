12a351afc46e0e66c7f001f24a10937785ae4e10
Sean Standen <standen.sean@gmail.com>
2022-03-17 11:10:02 -0700

    Added media files and inital page layout for Portfolio page.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
+//components
+import FullScreenDiv from '../components/UI/FullScreenDiv';
+import BGVideoDiv from '../components/UI/BGVideoDiv';
+
+//animations
+
+//media
+
+
 export default function Portfolio() {
   return (
-    <main>
+    <main className='autoScrollParent'>
         <Div>
-            
+            <section className='autoScrollChild'></section>
+            <section className='autoScrollChild'></section>
+            <section className='autoScrollChild'></section>
+            <section className='autoScrollChild'></section>
+            <section className='autoScrollChild'></section>
         </Div>
     </main>
   )
