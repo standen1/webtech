82c216c3aaa8499686ff6d83ff481a873507ad75
Sean Standen <standen.sean@gmail.com>
2022-03-17 13:13:21 -0700

    initial setup for Contact page as well as adding it to Routes in App.js

---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export default function Contact() {
+  return (
+    <main>
+        <Div>
+
+        </Div>
+    </main>
+  );
+}
+
+const Div = styled.div`
+
+`;
